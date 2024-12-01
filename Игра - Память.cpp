97a6ee5570b0723f61f7e8a2ab861957ecf0074a
Lazurit04D <61789684+Lazurit04D@users.noreply.github.com>
2024-12-01 23:32:14 +0300

    Игра доведена до играбельного состояния
    1. Добавлена рекурсивная функция открытия ячеек;
    2. Немного изменён InitGameField;
    3. Изменения по мелочи.

--- Игра - Память.cpp
+++ Игра - Память.cpp
@@ -1,17 +1,19 @@
 #include <time.h>
+#include <windows.h>
 #include <iostream>
 using namespace std;
 
 //Символы вынесены чтобы быть доступными для всей программы
-const char chars[8] = { '!', '@', '$', '%', '^', '&', '*', '0'};
+const char chars[8] = { '!', '@', '#', '$', '%', '^', '&', '*'};
+int countOfGuessed = 0;
 
 //Заполнение игрового поля
-void InitGameField(char gameField[][4], bool user)
+void InitGameField(char gameField[][4], int mode, const char secondField[][4] = nullptr)
 {
     int randInt = 0;
     int countOfAnyChar[8]; //Эта переменная хранит в себе количество каждого символа в игровом поле
     //Этим условием выполнена небольшая оптимизация, так как эта переменная не используется когда заполняется игровое поле игрока
-    if (!user)
+    if (mode == 2)
     {
         for (int i = 0; i < 8; i++)
         {
@@ -23,7 +25,7 @@ void InitGameField(char gameField[][4], bool user)
     {
         for (int j = 0; j < 4; j++)
         {
-            if (!user)
+            if (mode == 2)
             {
                 //Этот цикл не даёт повторяться одному символу больше 2 раз
                 while (true)
@@ -38,18 +40,24 @@ void InitGameField(char gameField[][4], bool user)
                 gameField[i][j] = chars[randInt];
                 countOfAnyChar[randInt]++;
             }
+            else if (mode == 1)
+            {
+                gameField[i][j] = secondField[i][j];
+            }
             else
             {
                 //Заполнение пустого игрового поля игрока
-                gameField[i][j] = '#';
+                gameField[i][j] = '?';
             }
         }
     }
 }
 
 //Вывод игрового поля в консоль
-void PrintGameField(const char gameField[][4])
+void PrintGameField(const char gameField[][4], int X = -1, int Y = -1)
 {
+    cout << "Количество угаданных: " << countOfGuessed << endl << endl;
+
     for (int i = 0; i < 4; i++)
     {
         for (int j = 0; j < 4; j++)
@@ -61,6 +69,43 @@ void PrintGameField(const char gameField[][4])
     }
 }
 
+void OpenCell(const char gameField[][4], char userGameField[][4], int previosX = -1, int previosY = -1)
+{
+    int X, Y;
+    char tempGameField[4][4] = {};
+    InitGameField(tempGameField, 1, userGameField);
+
+    PrintGameField(userGameField);
+    cout << "\nВведите X ячейки: ";
+    cin >> X;
+    cout << "Введите Y ячейки: ";
+    cin >> Y;
+
+    system("cls");
+    tempGameField[Y][X] = gameField[Y][X];
+    PrintGameField(tempGameField, X, Y);
+    Sleep(1000);
+    system("cls");
+
+    if (gameField[previosY][previosX] != gameField[Y][X])
+    {
+        OpenCell(gameField, userGameField, X, Y);
+    }
+    else
+    {
+        if (previosX == X && previosY == Y)
+        {
+            OpenCell(gameField, userGameField, X, Y);
+        }
+        else
+        {
+            userGameField[Y][X] = gameField[Y][X];
+            userGameField[previosY][previosX] = gameField[previosY][previosX];
+            countOfGuessed++;
+        }
+    }
+}
+
 int main()
 {
     setlocale(0, "");
@@ -69,11 +114,15 @@ int main()
     char gameField[4][4] = {};
     char userGameField[4][4] = {};
 
-    InitGameField(gameField, false);
-    PrintGameField(gameField);
+    InitGameField(userGameField, 0);
+    InitGameField(gameField, 2);
 
-    cout << endl;
+    while (countOfGuessed < 8)
+    {
+        OpenCell(gameField, userGameField);
+    }
 
-    InitGameField(userGameField, true);
     PrintGameField(userGameField);
+
+    return 0;
 }
