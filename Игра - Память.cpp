ac445c4942d478a0ee97cb8164a33fbe34537eb6
NasvayZero <149622297+NasvayZero@users.noreply.github.com>
2024-12-02 23:00:30 +0300

    Update Игра - Память.cpp
    1. Улучшена функция PrintGameField() и OpenCell();
    2. Во многих местах добавлены комментарии с пояснениями;
    3. Добавлена функция GameFieldCMP() (На данный момент не используется).

--- Игра - Память.cpp
+++ Игра - Память.cpp
@@ -1,11 +1,28 @@
 #include <time.h>
-#include <windows.h>
+#include <windows.h> //Для функции Sleep()
 #include <iostream>
 using namespace std;
 
 //Символы вынесены чтобы быть доступными для всей программы
-const char chars[8] = { '!', '@', '#', '$', '%', '^', '&', '*'};
+const char chars[8] = { '!', '@', '#', '$', '%', '^', '&', '*' };
 int countOfGuessed = 0;
+
+//Функция сравнения игровых полей (На данный момент не используется)
+int GameFieldCMP(const char gameField[][4], const char secondField[][4])
+{
+    int countOfMatches = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (gameField[i][j] == secondField[i][j])
+            {
+                countOfMatches++;
+            }
+        }
+    }
+    return countOfMatches;
+}
 
 //Заполнение игрового поля
 void InitGameField(char gameField[][4], int mode, const char secondField[][4] = nullptr)
@@ -42,6 +59,7 @@ void InitGameField(char gameField[][4], int mode, const char secondField[][4] = 
             }
             else if (mode == 1)
             {
+                //Копирование одного поле в другое
                 gameField[i][j] = secondField[i][j];
             }
             else
@@ -56,37 +74,66 @@ void InitGameField(char gameField[][4], int mode, const char secondField[][4] = 
 //Вывод игрового поля в консоль
 void PrintGameField(const char gameField[][4], int X = -1, int Y = -1)
 {
-    cout << "Количество угаданных: " << countOfGuessed << endl << endl;
+    cout << "Количество угаданных пар: " << countOfGuessed << endl << endl;
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 5; i++)
     {
-        for (int j = 0; j < 4; j++)
+        for (int j = 0; j < 5; j++)
         {
-            cout << gameField[i][j] << " ";
+            if (i == 0)
+            {
+                cout << "[" << j << "] ";
+            }
+            else
+            {
+                if (j == 0)
+                {
+                    cout << "[" << i << "] ";
+                }
+                else
+                {
+                    if (gameField[i - 1][j - 1] == '?')
+                    {
+                        cout << "|" << gameField[i - 1][j - 1] << "| ";
+                    }
+                    else
+                    {
+                        //Тернарный оператор определяет, какая ячейка была выбрана последней, для того, чтобы её выделить
+                        cout << (X + 1 == j && Y + 1 == i ? ">" : "[") << gameField[i - 1][j - 1] << (X + 1 == j && Y + 1 == i ? "< " : "] ");
+                    }
+                }
+            }
         }
 
-        cout << endl;
+        cout << endl << endl;
     }
 }
 
 void OpenCell(const char gameField[][4], char userGameField[][4], int previosX = -1, int previosY = -1)
 {
-    int X, Y;
+    unsigned int X, Y;
     char tempGameField[4][4] = {};
     InitGameField(tempGameField, 1, userGameField);
+    //Создание временного игрового поля и копирование в него уже существующего
 
+    //Ввод координат ячейки
     PrintGameField(userGameField);
-    cout << "\nВведите X ячейки: ";
+    cout << "Введите координату «X» у вашей ячейки\n(1-4, включительно): ";
     cin >> X;
-    cout << "Введите Y ячейки: ";
+    cout << "Введите координату «Y» у вашей ячейки\n(1-4, включительно): ";
     cin >> Y;
 
-    system("cls");
-    tempGameField[Y][X] = gameField[Y][X];
-    PrintGameField(tempGameField, X, Y);
+    X--;
+    Y--;
+    //Отсчёт идёт не от 0, а от 1
+    system("cls"); //Очиска консоли
+    tempGameField[Y][X] = gameField[Y][X]; //Копирование во временное игровое поле значение из основного игрового поля
+    PrintGameField(tempGameField, X, Y); //Вывод игрового поля в консоль
     Sleep(1000);
     system("cls");
+    //Очиска консоли спустя некоторое время
 
+    //Проверка на соответствие игрового поля с предыдущим игровым полем
     if (gameField[previosY][previosX] != gameField[Y][X])
     {
         OpenCell(gameField, userGameField, X, Y);
@@ -111,18 +158,25 @@ int main()
     setlocale(0, "");
     srand(time(nullptr));
 
+    //Создание 2-ух игровых полей
     char gameField[4][4] = {};
     char userGameField[4][4] = {};
 
+    //Заполнение игрового поля
     InitGameField(userGameField, 0);
     InitGameField(gameField, 2);
 
-    while (countOfGuessed < 8)
+    //Цикл игры
+    while (countOfGuessed != 8)
     {
         OpenCell(gameField, userGameField);
     }
 
+    //Окончание игры
     PrintGameField(userGameField);
+    cout << "Игра окончена!\n\n";
+    cout << "ВЫ УГАДАЛИ ВСЕ ЯЧЕЙКИ!\n\n";
+    cout << "Поздравляем!\n";
 
     return 0;
 }
